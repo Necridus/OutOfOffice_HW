3c4b903b944f2936fc278a89fadadcea7efae784
Martin Ádám Kimmel <martin.kimmel@stud.uni-corvinus.hu>
2021-11-14 14:57:26 +0100

    Added bootstrap to the solution and changed some classes

--- Login.php
+++ Login.php
@@ -7,13 +7,15 @@
         <meta charset="utf-8">
         <title>Login - Out Of Office</title>
         <link rel="stylesheet" href="style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </head>
     <body class="textCenter bodyBackground fontStyle">
         
         <div class="container">
 
             <div class="row">
-                <div class="col-12">
+                <div class="col-12 fw-bold text-uppercase">
                 <h1>
                     Jelentkezzen be a folytatáshoz!
                 </h1>       
@@ -22,17 +24,17 @@
             <div class="row">
                 <div class="col-12">
                     <form action="CheckLogin.php" method="POST">
-                        <table class="center">
-                            <tr class="margin5px">
-                                <td class="bold">
+                        <table class="text-center d-flex justify-content-center">
+                            <tr class="m-5">
+                                <td class="fw-bold text-uppercase text-end">
                                     Felhasználónév:
                                 </td>
                                 <td>
                                     <input type="text" name="username">
                                 </td>
                             </tr>
-                            <tr class="margin5px">
-                                <td class="bold">
+                            <tr class="m-2">
+                                <td class="fw-bold text-uppercase text-end">
                                     Jelszó:
                                 </td>
                                 <td>
@@ -40,7 +42,7 @@
                                 </td>
                             </tr>
                         </table>
-                        <input type="submit" class="buttonStyle margin5px marginTop50px" value="Bejelentkezés">
+                        <input type="submit" class="btn btn-primary m-2 mt-5 text-uppercase" value="Bejelentkezés">
                     </form>     
                 </div>
             </div>
