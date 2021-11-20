7c9b1d883e7b5dcb85b34fd2a062bec5ce5afa48
Martin Ádám Kimmel <martin.kimmel@stud.uni-corvinus.hu>
2021-11-20 16:17:11 +0100

    Created OutOfOfficeManager table with cond. color

--- OutOfOfficeManager.php
+++ OutOfOfficeManager.php
@@ -8,6 +8,7 @@
         header("Location:Login.php");
         exit;
     }
+    require_once('Connect.php');
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -18,6 +19,30 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="OOOstyle.css">
     </head>
+
+    <script>
+            function ChooseConditionalBGColor(rowNumber){
+               
+                var containerName = "StatusTD" + rowNumber;
+                var hiddenContainerName = "status" + rowNumber;
+
+                var status = document.getElementById(hiddenContainerName).value;
+
+				if (status == 'Függőben')
+				{
+                    document.getElementById(containerName).className = "bg-warning";
+				}
+				else if (status == 'Elfogadva')
+                {
+                    document.getElementById(containerName).className = "bg-success";
+				}
+                else if (status == 'Elutasítva')
+                {
+                    document.getElementById(containerName).className = "bg-danger";
+                }
+            }
+        </script>
+
     <body class="bodyBackground fontStyle">
 
         <div class="row d-flex justify-content-start fixed-top p-0 m-0">
@@ -25,6 +50,67 @@
                     < Vissza
                 </a>
         </div>
+        <div class="d-flex justify-content-center">
+            <div class="commonContainer rounded col-10">
+                    <h1 class="text-center text-uppercase fw-bold">
+                        Benyújtott szabadságok
+                    </h1>
+
+                    <?php
+                    $queryRequests = mysqli_query($connection, "SELECT Users.Name, Requests.StartDate, Requests.EndDate, Requests.Status FROM Requests JOIN Users ON Requests.UserID = Users.ID WHERE Requests.ValidTo IS NULL");
+					?>
+
+					<table class="col-12 table table-striped table-bordered table-hover text-center">
+						<tr class="thead-dark fw-bold text-uppercase">
+							<td>
+								Dolgozó
+							</td>
+							<td>
+								Szabadság kezdete
+							</td>
+							<td>
+								Szabadság vége
+							</td>
+                            <td colspan="2">
+								Állapot
+							</td>
+						</tr>
+						
+						<?php
+                            $rowNumber = 0;
+							while($row = mysqli_fetch_assoc($queryRequests))
+							{
+						?>
+						<tr>
+							<td>
+								<?php echo ($row['Name']); ?>
+							</td>
+							<td>
+								<?php echo ($row['StartDate']); ?>
+							</td>
+							<td>
+								<?php echo ($row['EndDate']); ?>
+							</td>
+                            <td id="<?php echo ("StatusTD$rowNumber");?>">
+								<?php echo ($row['Status']); ?>
+
+                                <input type="hidden" id="<?php echo ('status'.$rowNumber)?>" value="<?php echo ($row['Status']); ?>">
+                                
+							</td>
+                            <td>
+								<?php echo "Módosítás"; ?>
+							</td>
+						</tr>
+						
+						<?php
+                            echo ('<script type="text/javascript"> ChooseConditionalBGColor('.$rowNumber.'); </script>');
+                            $rowNumber = $rowNumber + 1;
+							}
+                        ?>
+						</table>
+						                    
+            </div>
+        <div>
         
         <div class="row d-flex justify-content-end fixed-bottom p-0 m-0">
                     <a href=Logout.php class="col-1 text-end btn btn-secondary fw-bold">
