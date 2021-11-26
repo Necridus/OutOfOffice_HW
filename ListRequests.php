1d49fdc09a0e37961384fe0ff1e78166cbffce66
Martin Ádám Kimmel <martin.kimmel@stud.uni-corvinus.hu>
2021-11-26 22:45:26 +0100

    Changed curdate() to now() when deleting request

--- ListRequests.php
+++ ListRequests.php
@@ -68,7 +68,7 @@ require_once('Connect.php');
                 //A függvény nem látja a globális változókat ($connection)
                 include('Connect.php');
                 $id = $_GET['requestToBeDeleted'];
-                $deleteQuery = "UPDATE Requests SET ValidTo = CURDATE() WHERE ID=$id";
+                $deleteQuery = "UPDATE Requests SET ValidTo = NOW() WHERE ID=$id";
                 if ($connection->query($deleteQuery) === TRUE) {
                     echo "<script>alert('Szabadság kérelem eltávolítva')</script>";
                   } else {
