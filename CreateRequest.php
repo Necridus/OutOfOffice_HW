28ed837cc090318fc33c362f1044a33dd627ac54
Attila Radics <attila.radics@sap.com>
2021-11-27 12:52:24 +0100

    email küldés (kikommentelve) + űrlapvalidálás finally kész --attila

--- CreateRequest.php
+++ CreateRequest.php
@@ -6,8 +6,7 @@ session_start();
 if (!$_SESSION['Loggedin']) {
     header("Location:Login.php");
     exit;
-}
-elseif ($_SESSION['IsAdmin']) {
+} elseif ($_SESSION['IsAdmin']) {
     header("Location:Login.php?noRights=true");
     exit;
 }
@@ -17,7 +16,7 @@ require_once('Connect.php');
 <html>
 
 <head>
-<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
     <meta charset="utf-8">
     <title>Create Request - Out Of Office</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -31,12 +30,13 @@ require_once('Connect.php');
         if (startDate == "" || endDate == "") {
             alert("A dátumokat ki kell tölteni");
             return false;
-        } else if(startDate > endDate) {
+        } else if (startDate > endDate) {
             alert("Start date must be earlier than end date");
             return false;
-        } 
+        }
     }
 </script>
+
 <body class="bodyBackground fontFormat fw-bold">
 
     <div class="row d-flex justify-content-start fixed-top p-0 m-0">
@@ -52,60 +52,84 @@ require_once('Connect.php');
         <div class="commonContainer rounded col-10">
             <h2 class="text-secondary fw-bold">Hello, <?php echo $_SESSION['Username'] ?>!</h2>
             <h1 class="text-center fw-bold mb-5">Új szabadság létrehozása</h1>
-            <form name="myForm" method="post"  onsubmit="return validateForm()" action="<?php print $_SERVER['PHP_SELF']?>" class="d-flex justify-content-evenly">
+            <form name="myForm" method="post" onsubmit="return validateForm()" action="<?php print $_SERVER['PHP_SELF'] ?>" class="d-flex justify-content-evenly">
                 <p>Szabadság kezdete: <input type="date" name="startDate"></p>
                 <p>Szabadság vége: <input type="date" name="endDate"></p>
                 <p><input type="submit" value="Küld"></p>
             </form>
 
-    <?php
-        $username = $_SESSION['Username'];
-        function getArray() {
-            //A függvény nem látja a globális változókat ($connection, $username)
-            include('Connect.php');
-            $username = $_SESSION['Username'];
-            $queryMyRequests = mysqli_query($connection, "SELECT Requests.StartDate, Requests.EndDate FROM Requests JOIN Users ON Requests.UserID = Users.ID WHERE Users.Username = '" . $username . "' AND Requests.ValidTo IS NULL ORDER BY Requests.ID");
-            $requests = [];
-            if(mysqli_num_rows($queryMyRequests) > 0){  
-                while($row = mysqli_fetch_assoc($queryMyRequests)){
-                    array_push($requests,$row);
+            <?php
+            function getAdmins() {
+                include('Connect.php');
+                return mysqli_query($connection,"SELECT Users.EmailAdress, Users.UserName FROM Users WHERE isAdmin = 1");
+            }
+            
+            function sendMailAboutNewRequest($adminEmails, $startDate, $endDate, $username){
+                while ($row = mysqli_fetch_assoc($adminEmails)) {
+                    $sendMail = mail($row['EmailAdress'],"New Request has been created", "Dear ".$row['UserName']."! \n\n New Out Of Office Request has been created by ".$username." from ".$startDate." to ".$endDate."! \n Log in to http://portalbce.hu/DF9YEV/OutOfOffice_HW/Login.php to see the Request. \n\n This is an automated message sent by the server, please don't respond to this.");
                 }
+                if( $sendMail == true ) {
+                    echo '<script>alert("Message was sent successfully!")</script>';
+                 }else {
+                    echo '<script>alert("Message could not be sent, there is probably no e-mail adress added to this user!")</script>';
+                 }
             }
-            return $requests;
-        }
-        //TO DO
-        //Megvizsgálja, hogy az adott kezdő és befejező dátummal rendelkező szabadság átfedésben van-e egy másikkal
-        function hasOverlap($newStartDate, $newEndDate, $requests){
-            for($i=0;$i<count($requests);$i++){
-                $start = $requests[$i]['StartDate'];
-                $end = $requests[$i]['EndDate'];
-                if($newStartDate <= $end && $newEndDate >= $start){
-                    return true;
-                }
-            }
-            return false;
-        }
-        
-        if (!empty($_POST)){
-            $startDate = $_POST['startDate'];
-            $endDate = $_POST['endDate'];
-            $validFrom = date("Y-m-d H:i:s");
-            $userID = $connection->query("SELECT Users.ID FROM Users WHERE UserName = '$username'")->fetch_object()->ID;
-            $result = mysqli_query($connection, "INSERT INTO Requests (UserID, StartDate, EndDate, Status, ValidFrom, ValidTo) VALUES ('$userID','$startDate','$endDate','Függőben','$validFrom', NULL)");
-            if($result)
-            {
-                ?><p><?php
-                echo hasOverlap($startDate, $endDate, getArray());
-                ?></p><?php
-            }
-            else
-            {
-                echo "<script>alert('Nem sikerült továbbítani a kérést az adatbázis számára')</script>";
-            }
-        }
-    ?>
 
-</div>
+            $username = $_SESSION['Username'];
+
+            //Visszaad egy tömböt, amelynek az elemei asszociatív tömbök a szabadságok kezdő és végdátumaival
+            function getRequests()
+            {
+                //A függvény nem látja a globális változókat ($connection, $username)
+                include('Connect.php');
+                $username = $_SESSION['Username'];
+                $queryMyRequests = mysqli_query($connection, "SELECT Requests.StartDate, Requests.EndDate FROM Requests JOIN Users ON Requests.UserID = Users.ID WHERE Users.Username = '" . $username . "' AND Requests.ValidTo IS NULL ORDER BY Requests.ID");
+                $requests = [];
+                if (mysqli_num_rows($queryMyRequests) > 0) {
+                    while ($row = mysqli_fetch_assoc($queryMyRequests)) {
+                        array_push($requests, $row);
+                    }
+                }
+                return $requests;
+            }
+            //TO DO
+            //Megvizsgálja, hogy az adott kezdő és befejező dátummal rendelkező szabadság átfedésben van-e egy másikkal
+            function hasOverlap($newStartDate, $newEndDate, $requests)
+            {
+                for ($i = 0; $i < count($requests); $i++) {
+                    $start = $requests[$i]['StartDate'];
+                    $end = $requests[$i]['EndDate'];
+                    //print("A megadott kezdődátum: ".$newStartDate.", a megadott végdátum: ".$newEndDate."<br>");
+                    //print("A vizsgált kezdődátum: ".$start.", a vizsgált végdátum: ".$end."<br>");
+                    //print("Átfedés: ");
+                    //print((($newStartDate <= $end && $newEndDate >= $start)? "van" : "nincs"));
+                    if ($newStartDate <= $end && $newEndDate >= $start) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            if (!empty($_POST)) {
+                $startDate = $_POST['startDate'];
+                $endDate = $_POST['endDate'];
+                $validFrom = date("Y-m-d H:i:s");
+                $userID = $connection->query("SELECT Users.ID FROM Users WHERE UserName = '$username'")->fetch_object()->ID;
+                if (!hasOverlap($startDate, $endDate, getRequests())) {
+                    $result = mysqli_query($connection, "INSERT INTO Requests (UserID, StartDate, EndDate, Status, ValidFrom, ValidTo) VALUES ('$userID','$startDate','$endDate','Függőben','$validFrom', NULL)");
+                    if ($result) {
+                        echo "<script>alert('Szabadság kérelem hozzáadva')</script>";
+                        //sendMailAboutNewRequest(getAdmins(),$startDate,$endDate,$username);
+                    } else {
+                        echo "<script>alert('Nem sikerült továbbítani a kérést az adatbázis számára')</script>";
+                    }
+                } else{
+                    echo "<script>alert('Ez a szabadság átfedésben van egy másikkal')</script>";
+                }
+            }
+            ?>
+
+        </div>
     </div>
 
     <div class="row d-flex justify-content-end fixed-bottom p-0 m-0">
